e53620003231fcd16690fb44cf18256feb043fc4
Yasuaki Kudo <yasu@yasuaki.com>
2025-08-30 19:22:44 +0900

    Remove language switch UI

--- BlazorWP/Data/LanguageService.cs
+++ BlazorWP/Data/LanguageService.cs
@@ -7,8 +7,6 @@ namespace BlazorWP.Data
         private CultureInfo _currentCulture = new("en-US");
         public CultureInfo CurrentCulture => _currentCulture;
 
-        public event Action? OnChange;
-
         public void SetCulture(string cultureCode)
         {
             var culture = new CultureInfo(cultureCode);
@@ -17,19 +15,6 @@ namespace BlazorWP.Data
             CultureInfo.DefaultThreadCurrentCulture = culture;
             CultureInfo.DefaultThreadCurrentUICulture = culture;
             _currentCulture = culture;
-            OnChange?.Invoke();
-        }
-
-        public void ToggleCulture()
-        {
-            if (_currentCulture.Name == "ja-JP")
-            {
-                SetCulture("en-US");
-            }
-            else
-            {
-                SetCulture("ja-JP");
-            }
         }
     }
 }
