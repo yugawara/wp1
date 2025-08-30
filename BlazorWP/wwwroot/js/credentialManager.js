983ecd0cdd4a78ce9a37f857d10d34e1b5ecde36
Yasuaki Kudo <yasu@tng.coop>
2025-08-30 17:30:37 +0900

    a

--- BlazorWP/wwwroot/js/credentialManager.js
+++ BlazorWP/wwwroot/js/credentialManager.js
@@ -1,9 +1,12 @@
 export async function storeCredentials(username, password) {
+  console.log('Storing credentials for user:', username);
   if ('credentials' in navigator && window.PasswordCredential) {
     try {
+      console.log('Using Credential Management API to store credentials.');
       const cred = new PasswordCredential({ id: username, password: password });
       await navigator.credentials.store(cred);
     } catch {
+      console.warn('Failed to store credentials using Credential Management API.');
       // ignore errors
     }
   }
