976c6e360e4fe07042d4a9aeb9b88fe447e68a67
Yasuaki Kudo <yasu@yasuaki.com>
2025-09-02 19:26:00 +0900

    Refresh entire app when AppFlags change

--- BlazorWP/Data/AppFlags.cs
+++ BlazorWP/Data/AppFlags.cs
@@ -24,19 +24,26 @@ namespace BlazorWP.Data
         public AuthType Auth { get; private set; } = AuthType.Jwt;
         public Language Language { get; private set; } = Language.English;
 
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
         public void SetAppMode(AppMode mode)
         {
             Mode = mode;
+            NotifyStateChanged();
         }
 
         public void SetAuthMode(AuthType auth)
         {
             Auth = auth;
+            NotifyStateChanged();
         }
 
         public void SetLanguage(Language language)
         {
             Language = language;
+            NotifyStateChanged();
         }
     }
 }
