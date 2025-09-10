29448b83d666207c47b5b8782d3c0de6ad79fa23
Yasuaki Kudo <yasu@tng.coop>
2025-09-10 19:27:59 +0900

    f

--- BlazorWP.E2E/Fixtures/E2ECollection.cs
+++
@@ -1,4 +0,0 @@
-using Xunit;
-namespace BlazorWP.E2E;
-[CollectionDefinition("e2e")]
-public sealed class E2ECollection : ICollectionFixture<BrowserFixture>, ICollectionFixture<WordPressFixture> { }
