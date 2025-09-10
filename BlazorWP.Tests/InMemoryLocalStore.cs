82e6171f90b9047fa117f970a2cfad6dff0206e0
Yasuaki Kudo <yasu@tng.coop>
2025-09-10 09:04:48 +0900

    a

--- BlazorWP.Tests/InMemoryLocalStore.cs
+++ BlazorWP.Tests/InMemoryLocalStore.cs
@@ -1,7 +1,3 @@
-using System;                      // for StringComparer, Guid
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;      // <-- needed
 using BlazorWP.Data;
 
 namespace BlazorWP.Tests;
