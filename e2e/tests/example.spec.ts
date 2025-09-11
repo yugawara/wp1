13d50f3f32f460549fed037ee2f1e6e90c91b8df
Yasuaki Kudo <yasu@tng.coop>
2025-09-11 11:11:06 +0900

    s

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test.skip('has title', async ({ page }) => {
   await page.goto('/../');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/wptest/);
 });
-test.skip('has title2', async ({ page }) => {
-  await page.goto('/blazorapp');
+test('has title2', async ({ page }) => {
+  await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Home/);
